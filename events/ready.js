3f7c8df926b906b02ec8c645c8ed195a8f63dd6a
xiaopan42 <junrui0422@gmail.com>
2025-04-09 23:30:19 +0800

    comit

--- events/ready.js
+++ events/ready.js
@@ -3,6 +3,6 @@ module.exports = {
     once: true, // 設置 once 為 true，表示這個事件只會觸發一次
     execute(client) {
         console.log(`✅ ${client.user.tag} 已上線！`);
-        client.user.setActivity('Rust', { enum: 'PLAYING' });
+        client.user.setActivity('open everyone bot v1.0', { enum: 'PLAYING' });
     }
 };
